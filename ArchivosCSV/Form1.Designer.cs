2cd5cb407675611c47f92d8b38aed5d536724318
XxLunaxX <josellunag2909@gmail.com>
2026-02-20 18:12:26 -0600

    Agregar archivos de proyecto.

---
+++ ArchivosCSV/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ArchivosCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCrear = new Button();
+            btnAbrir = new Button();
+            btnCambios = new Button();
+            btnBuscar = new Button();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            txtBuscar = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtNombre = new TextBox();
+            btnAgregar = new Button();
+            btnEliminar = new Button();
+            txtCarrera = new TextBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            btnCrear.Location = new Point(529, 10);
+            btnCrear.Name = "btnCrear";
+            btnCrear.Size = new Size(94, 29);
+            btnCrear.TabIndex = 0;
+            btnCrear.Text = "Crear";
+            btnCrear.UseVisualStyleBackColor = true;
+            btnCrear.Click += btnCrear_Click;
+            // 
+            // btnAbrir
+            // 
+            btnAbrir.Location = new Point(629, 11);
+            btnAbrir.Name = "btnAbrir";
+            btnAbrir.Size = new Size(94, 29);
+            btnAbrir.TabIndex = 1;
+            btnAbrir.Text = "Abrir";
+            btnAbrir.UseVisualStyleBackColor = true;
+            btnAbrir.Click += btnAbrir_Click;
+            // 
+            // btnCambios
+            // 
+            btnCambios.Location = new Point(729, 10);
+            btnCambios.Name = "btnCambios";
+            btnCambios.Size = new Size(94, 29);
+            btnCambios.TabIndex = 2;
+            btnCambios.Text = "Cambios";
+            btnCambios.UseVisualStyleBackColor = true;
+            btnCambios.Click += btnCambios_Click;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(328, 12);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(94, 29);
+            btnBuscar.TabIndex = 3;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            btnBuscar.Click += btnBuscar_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 115);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(507, 238);
+            dataGridView1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(12, 359);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.Size = new Size(507, 80);
+            dataGridView2.TabIndex = 5;
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Location = new Point(70, 12);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.Size = new Size(205, 27);
+            txtBuscar.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Buscar";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(70, 48);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(205, 27);
+            txtNombre.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(629, 51);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(94, 29);
+            btnAgregar.TabIndex = 10;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(729, 51);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(94, 29);
+            btnEliminar.TabIndex = 11;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // txtCarrera
+            // 
+            txtCarrera.Location = new Point(70, 81);
+            txtCarrera.Name = "txtCarrera";
+            txtCarrera.Size = new Size(205, 27);
+            txtCarrera.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 84);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 20);
+            label3.TabIndex = 12;
+            label3.Text = "Carrera";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(844, 450);
+            Controls.Add(txtCarrera);
+            Controls.Add(label3);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtNombre);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtBuscar);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnBuscar);
+            Controls.Add(btnCambios);
+            Controls.Add(btnAbrir);
+            Controls.Add(btnCrear);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCrear;
+        private Button btnAbrir;
+        private Button btnCambios;
+        private Button btnBuscar;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private TextBox txtBuscar;
+        private Label label1;
+        private Label label2;
+        private TextBox txtNombre;
+        private Button btnAgregar;
+        private Button btnEliminar;
+        private TextBox txtCarrera;
+        private Label label3;
+    }
+}
